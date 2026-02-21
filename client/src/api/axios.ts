21370df7cecdf9e032249b1e188f9dcc57118e71
Sam Robert <91603698+bobTheBuilderCodes@users.noreply.github.com>
2026-02-21 14:04:19 +0000

    Prod fixes

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api";
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  import.meta.env.VITE_API_BASE_URL ||
+  (import.meta.env.PROD
+    ? "https://albc-cms-server.onrender.com/api"
+    : "http://localhost:5001/api");
 
 const API = axios.create({
   baseURL,
